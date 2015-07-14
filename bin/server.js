ab63696436727c229b00051916063a0eaba3accd
Nicolas Jaremek <nicolas.jaremek@intelygenz.com>
2015-07-14 09:39:25 +0200

    Added Mongoose and basic configuration for server startup.

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,30 @@
-var service = require('../lib/service');
 var config = require('config');
+var mongoose = require('mongoose');
+
+var log = require('../lib/util/logger');
+var service = require('../lib/service');
 
 var options = {
   port: process.env.PORT || config.get('port')
 };
 
-service(options).start(function(){
-  console.log('Started server');
+/* MongoDB */
+env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  mongoose.set('debug', true);
+}
+
+log.info('Connecting to MongoDB...');
+mongoose.connect(config.get('db.conn'), function(err) {
+  if (err) throw err;
+  log.info('Connected to MongoDB');
+
+  mongoose.connection.on('error', function(err) {
+    log.error(err);
+  });
+
+  /* Server */
+  service(options).start(function() {
+    console.log('Started server');
+  });
 });
