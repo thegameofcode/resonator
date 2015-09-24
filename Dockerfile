0fa10969f4266ae99c6de1fe2553108f14da06a7
Nicolas Jaremek <nicolas.jaremek@intelygenz.com>
2015-09-24 11:31:59 +0200

    Added Mocha tests for single target email endpoint middleware.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM nodesource/trusty:0.12.3
+FROM nodesource/node:4.1
 
 WORKDIR /app
 ADD package.json /app/package.json
 ADD . /app
-RUN cd /app
 RUN npm install
 
 
--- bin/server.js
+++ bin/server.js
@@ -16,7 +16,6 @@ if (env === 'development') {
 
 log.info('Connecting to MongoDB...');
 mongoose.connect(config.get('db.conn'), function(err) {
-  console.log('Error', err);
   if (err) throw err;
   log.info('Connected to MongoDB');
 
